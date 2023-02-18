58fc85eacd7f3904dced9e7c1929077da4147f10
Fedor Sharov <1710193@mail.ru>
2023-02-18 14:43:32 +0300

    fixes tests

--- src/test/java/ru/netology/test/ReplenishmentTest.java
+++ src/test/java/ru/netology/test/ReplenishmentTest.java
@@ -1,7 +1,5 @@
 package ru.netology.test;
 
-import com.codeborne.selenide.Condition;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,9 +8,7 @@ import org.junit.jupiter.api.Test;
 import ru.netology.page.DashboardPage;
 import ru.netology.page.LoginPage;
 import ru.netology.data.DataHelper;
-import ru.netology.page.TransferPage;
 
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 class ReplenishmentTest {
@@ -72,10 +68,10 @@ class ReplenishmentTest {
         var mainPage = mainPage();
         balanceCard0001 = mainPage.getCardBalance("0001");
         balanceCard0002 = mainPage.getCardBalance("0002");
-        mainPage.replenishCard0001();
-        var transfer = new TransferPage().transfer(5000, DataHelper.cardNumber("0001"));
-        actualBalanceCard0001 = transfer.getCardBalance("0001");
-        actualBalanceCard0002 = transfer.getCardBalance("0002");
+        var transfer = mainPage.replenishCard0001();
+        transfer.transfer(5000, DataHelper.cardNumber("0001"));
+        actualBalanceCard0001 = mainPage.getCardBalance("0001");
+        actualBalanceCard0002 = mainPage.getCardBalance("0002");
 
         Assertions.assertEquals(balanceCard0001 + 5000, actualBalanceCard0001);
         Assertions.assertEquals(balanceCard0002 - 5000, actualBalanceCard0002);
@@ -86,11 +82,11 @@ class ReplenishmentTest {
         var mainPage = mainPage();
         balanceCard0001 = mainPage.getCardBalance("0001");
         balanceCard0002 = mainPage.getCardBalance("0002");
-        mainPage.replenishCard0002();
+        var transfer = mainPage.replenishCard0002();
 
-        var transfer = new TransferPage().transfer(500, DataHelper.cardNumber("0002"));
-        actualBalanceCard0001 = transfer.getCardBalance("0001");
-        actualBalanceCard0002 = transfer.getCardBalance("0002");
+        transfer.transfer(500, DataHelper.cardNumber("0002"));
+        actualBalanceCard0001 = mainPage.getCardBalance("0001");
+        actualBalanceCard0002 = mainPage.getCardBalance("0002");
 
         Assertions.assertEquals(balanceCard0001 - 500, actualBalanceCard0001);
         Assertions.assertEquals(balanceCard0002 + 500, actualBalanceCard0002);
@@ -101,9 +97,8 @@ class ReplenishmentTest {
         var mainPage = mainPage();
         balanceCard0001 = mainPage.getCardBalance("0001");
         balanceCard0002 = mainPage.getCardBalance("0002");
-        mainPage.replenishCard0001();
+        var transfer = mainPage.replenishCard0001();
 
-        var transfer = new TransferPage();
         transfer.transfer(100000, DataHelper.cardNumber("0001"));
         transfer.getErrorMassage("Ошибка! На карте **** **** **** 0001 не достаточно денег");
 
